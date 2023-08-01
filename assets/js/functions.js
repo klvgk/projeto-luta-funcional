ecc126fb9bee2b1939090cdf1954d6459ab9c96a
klvgk <klv.gonzaga@gmail.com>
2023-08-01 15:23:40 -0300

    Criação da lógica dos ataques/defesas

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -93,18 +93,36 @@ const stage = {
     //Atualiza os elementos da tela;
     update() {
         //Fighter1;
-        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life}HP`;
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(1)}HP`;
         let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
         this.fighter1El.querySelector('.bar').style.width = `${f1Pct}%`;
         //Fighter2;
-        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life}HP`;
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)}HP`;
         let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
         this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`;
     },
 
     //Lógica por trás do calculo de Ataque e Defesa;
     doAttack(attacking, attacked) {
-        console.log(`${attacking.name} atacando ${attacked.name}`);
+        if(attacking.life <= 0 || attacked.life <= 0) {
+            console.log('Respeite os mortos');
+            return;
+        }
+
+        const attackFactor = (Math.random() * 2).toFixed(2);
+        const defenseFactor = (Math.random() * 2).toFixed(2);
+
+        const actualAttack = attacking.attack * attackFactor;
+        const actualDefense = attacked.defense * defenseFactor;
+
+        if(actualAttack > actualDefense) {
+            attacked.life -= actualAttack;
+            attacked.life = attacked.life < 0 ? 0 : attacked.life;
+            console.log(`${attacking.name} causou ${actualAttack} de Dano em ${attacked.name}..`);
+        }else{
+            console.log(`${attacked.name} defendeu o ataque de ${attacking.name}..`);
+        }
+
 
         this.update();
     }
